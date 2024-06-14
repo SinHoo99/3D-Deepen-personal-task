2946dcdb7cc87300d1a38cf3314a3ebd31f612f6
SinHoo99 <tjdqls991218@gmail.com>
2024-06-14 16:29:00 +0900

    Init

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IState
+{
+    public void Enter();
+    public void Exit();
+    public void Update();
+    public void PhysicUpdate();
+}
+
+public abstract class StateMachine
+{
+    protected IState currentState;
+
+    public void ChangeState(IState state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState?.Enter();
+    }
+
+    public void Update()
+    {
+        currentState?.Update();
+    }
+
+    public void PhysicUpdate()
+    {
+        currentState?.PhysicUpdate();
+    }
+}
